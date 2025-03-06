9368816bf8940a67e3beb317c5c52e026c5fb634
Lucas Ferreira <155773773+lucasferreiradev0@users.noreply.github.com>
2025-03-06 18:18:57 -0300

    Gerador de Senha Aleatórias

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,3 @@
-
 function geradorSenha() {
     const qtdCaracter = document.querySelector('#qtdeCaracteres').value
     const upperCase = document.querySelector('#uppercase').checked
@@ -18,54 +17,135 @@ function geradorSenha() {
     if (simbolos) senhaChecked += symbols
 
     if(senhaChecked === '') {
-        const erroChecked = document.createElement('div')
-        erroChecked.innerHTML = '<i class="fa-solid fa-circle-exclamation"></i> Selecione como deseja que sua senha seja gerada!'
-        erroChecked.style.position = 'absolute'
-        erroChecked.style.top = '-50%'
-        erroChecked.style.left = '50%'
-        erroChecked.style.opacity = '0'
-        erroChecked.style.transform = 'translate(-50%)'
-        erroChecked.style.backgroundColor = '#D92525'
-        erroChecked.style.padding = '10px'
-        erroChecked.style.borderRadius = '5px'
-        erroChecked.style.color = '#fff'
-        erroChecked.style.transition = 'top .7s ease-out, opacity .7s ease'
-        
-        setTimeout(() => {
-            
-            erroChecked.style.opacity = '1'
-            erroChecked.style.top = '5%'
-        }, 10);
-
-        setTimeout(() => {
-            erroChecked.style.top = '-50%'
-            erroChecked.style.opacity = '0'
-            erroChecked.style.transition = 'top 2s ease-out, opacity 2s ease'
-        }, 1700)
-        document.body.appendChild(erroChecked)
-
-        setTimeout(() => {
-            document.body.removeChild(erroChecked)
-        }, 2400)
-
+        exibirAlert('<i class="fa-solid fa-triangle-exclamation fa-bounce"></i> Selecione como deseja que sua senha seja gerada!', '#D92525')
         return
     }
 
     let senha = ''
-
-    for(let i = 0; i < senhaChecked.length; i++){
+    for(let i = 0; i < qtdCaracter; i++){
         senha += senhaChecked.charAt(Math.floor(Math.random() * senhaChecked.length))
     }
-    
-    document.getElementById('password').innerHTML = senha
+
+    document.getElementById('password').value = senha
+    checkSenha(senha)
 }
 
-
-setInterval(() => { // teste muito louco
-    geradorSenha()
-}, 500)
-
-document.querySelector('#btn_gerar').addEventListener('click', () => {
-    geradorSenha()
+function copiarSenha() {
+    let senhaCopy = document.getElementById('password')
+    senhaCopy.select()
+    document.execCommand('copy')
+    exibirAlert('<i class="fa-regular fa-bell fa-shake"></i> Senha copiada com sucesso!', '#005C53')
+}
+document.querySelector('#btn_copy').addEventListener('click', () => {
+    copiarSenha()
 })
 
+function checkSenha(senha) {
+    let senhaCheck = 'fraca'
+    if(senha.length > 8 && /[A-Z]/.test(senha) && /[0-9]/.test(senha) && /[^a-zA-Z0-9]/.test(senha)) {
+        senhaCheck = 'forte'
+    } else if(senha.length > 6) {
+        senhaCheck = 'media'
+    }
+
+    if(senhaCheck === 'forte') {
+        exibirCheckSenha('#005C53', '#fff', 'Senha Forte')
+    }else if(senhaCheck === 'media'){
+        exibirCheckSenha('#F2A71B', '#fff', 'Senha Média')
+    }else {
+        exibirCheckSenha('#8C1F28', '#fff', 'Senha Fraca')
+    }
+}
+
+function exibirCheckSenha(bgcolor, colorText, texto){
+    let boxCheck = document.getElementById('checkSenha')
+    let check = document.createElement('div')
+    check.innerHTML = texto
+    check.style.backgroundColor = bgcolor
+    check.style.color = colorText
+    check.style.padding = '4px 10px'
+    check.style.borderRadius = '5px'
+    check.style.boxShadow = `0 0 8px ${bgcolor}`
+    check.style.marginBottom = '5px'
+    check.style.textTransform = 'uppercase'
+    check.style.fontSize = '.9rem'
+    
+    boxCheck.appendChild(check)
+    setTimeout(() => {
+        boxCheck.removeChild(check)
+    }, 5000);
+}
+
+function exibirAlert(alert, corAlert) {
+    const alertSenha = document.createElement('div')
+        alertSenha.innerHTML = alert
+        alertSenha.style.position = 'absolute'
+        alertSenha.style.top = '-50%'
+        alertSenha.style.left = '50%'
+        alertSenha.style.opacity = '0'
+        alertSenha.style.transform = 'translate(-50%)'
+        alertSenha.style.backgroundColor = corAlert
+        alertSenha.style.padding = '10px'
+        alertSenha.style.borderRadius = '5px'
+        alertSenha.style.color = '#fff'
+        alertSenha.style.transition = 'top .7s ease-out, opacity .7s ease'
+      
+    setTimeout(() => {
+        alertSenha.style.opacity = '1'
+        alertSenha.style.top = '5%'
+    }, 10);
+    setTimeout(() => {
+        alertSenha.style.top = '-50%'
+        alertSenha.style.opacity = '0'
+        alertSenha.style.transition = 'top 3s ease-out, opacity 3s ease'
+    }, 1700);
+
+    document.body.appendChild(alertSenha)
+    setTimeout(() => {
+        document.body.removeChild(alertSenha)
+    }, 2400);
+}
+    
+const btnGerar = document.getElementById('btn_gerar')
+btnGerar.onclick = function() {
+    geradorSenha()
+    let upperCase = document.querySelector('#uppercase').checked
+    let lowerCase = document.querySelector('#lowercase').checked
+    let numeros = document.querySelector('#numeros').checked
+    let simbolos = document.querySelector('#simbolos').checked
+    if(upperCase == true || lowerCase == true || numeros == true || simbolos == true) {
+        btnGerar.disabled = true
+        let infoGerar = document.createElement('div')    
+            infoGerar.style.position = 'absolute'
+            infoGerar.style.bottom = '-50%'
+            infoGerar.style.left = '50%'
+            infoGerar.style.transform = 'translate(-50%)'
+            infoGerar.style.opacity = '0'
+            infoGerar.style.backgroundColor = '#D95F43'
+            infoGerar.style.color = '#fff'
+            infoGerar.style.borderRadius = '5px'
+            infoGerar.style.padding = '5px 10px'
+            infoGerar.style.transition = 'bottom .7s ease-out, opacity .7s ease'
+            let time = 4
+            let contador = setInterval(() => {
+                infoGerar.innerHTML = `<i class="fa-solid fa-triangle-exclamation fa-bounce"></i> Aguarde ${time} para gerar uma nova senha!`
+                time--
+            }, 1000)
+            
+        setTimeout(() => {
+            infoGerar.style.opacity = '1'
+            infoGerar.style.bottom = '5%'
+        }, 40)
+
+        document.body.appendChild(infoGerar)    
+        setTimeout(() =>{
+            clearInterval(contador)
+            infoGerar.style.bottom = '-50%'
+            infoGerar.style.opacity = '0'
+            infoGerar.style.transition = 'bottom 3s ease-out, opacity 3s ease'
+            document.body.removeChild(infoGerar)
+            btnGerar.disabled = false
+        }, 5000);
+
+    }
+}
